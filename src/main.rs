869b73253f52128b4da3d41ecff888ae869814a3
Sun Lit <lessball@hotmail.com>
2022-01-10 17:42:56 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,45 @@ mod snake_move;
 use snake_move::*;
 
 const RADIUS: f32 = 30.0;
-const RADIUS_HARD: f32 = 25.0;
 const DISTANCE: f32 = 80.0;
+const SPEED: f32 = 300.0;
 
+#[derive(Component)]
 struct Leader {
-    snake: SnakeMove,
+    snake_head: SnakeHead,
     followers: Vec<Entity>,
 }
 
 impl Leader {
     pub fn new(followers: Vec<Entity>) -> Self {
-        let mut snake = SnakeMove::new();
-        snake.reset(Vec2::ZERO, Vec2::X, 0.0);
+        let mut snake_head = SnakeHead::new();
+        snake_head.reset(Vec2::ZERO, 0.0);
         Leader {
-            snake,
+            snake_head,
             followers,
         }
     }
 }
 
-struct Follower {}
+#[derive(Component)]
+struct Follower {
+    snake_body: SnakeBody,
+}
 
 impl Follower {
-    pub fn new() -> Self {
-        Follower {}
+    pub fn new(delay: f32, distance: f32, position: Vec2) -> Self {
+        Follower {
+            snake_body: SnakeBody::new(delay, distance, position),
+        }
     }
 }
 
-fn update(
+fn leader_move(
     time: Res<Time>,
     windows: Res<Windows>,
     keyboard_input: Res<Input<KeyCode>>,
     mousebutton_input: Res<Input<MouseButton>>,
-    query_leader: Query<(Entity, &mut Leader)>,
-    mut query_trans: Query<&mut Transform>,
+    mut query_leader: Query<(&mut Leader, &mut Transform)>,
 ) {
     let mut input_dir = Vec2::ZERO;
     if keyboard_input.pressed(KeyCode::W) {
@@ -59,11 +64,8 @@ fn update(
             cursor_pos = Some(p - Vec2::new(win.width() as f32, win.height() as f32) * 0.5);
         }
     }
-    query_leader.for_each_mut(|(entity, mut leader)| {
-        let mut leader_pos = query_trans
-            .get_mut(entity)
-            .map_or(Vec2::ZERO, |tm| tm.translation.truncate());
-        const SPEED: f32 = 300.0;
+    for (mut leader, mut tm) in query_leader.iter_mut() {
+        let mut leader_pos = tm.translation.truncate();
         let mut leader_dir = input_dir;
         if mousebutton_input.pressed(MouseButton::Left) {
             if let Some(p) = cursor_pos {
@@ -78,118 +80,84 @@ fn update(
         let delta_time = time.delta_seconds();
         let leader_delta = leader_dir * (delta_time * SPEED);
         leader_pos += leader_delta;
-        if let Ok(mut tm) = query_trans.get_mut(entity) {
-            tm.translation = leader_pos.extend(0.0);
-        }
-        let now = time.seconds_since_startup() as f32;
-        leader.snake.record(leader_pos, now);
+        tm.translation = leader_pos.extend(0.0);
+        leader
+            .snake_head
+            .move_head(leader_pos, time.seconds_since_startup() as f32);
+    }
+}
 
-        let mut positions: Vec<_> = leader
+fn follower_move(
+    time: Res<Time>,
+    query_leader: Query<&Leader>,
+    mut query_follower: Query<(&mut Follower, &mut Transform)>,
+) {
+    for leader in query_leader.iter() {
+        let mut bodys: Vec<_> = leader
             .followers
             .iter()
             .map(|e| {
-                query_trans.get_mut(*e).map_or(Vec2::ZERO, |tm| tm.translation.truncate())
+                query_follower
+                    .get(*e)
+                    .map(|(f, _)| f.snake_body.clone())
+                    .unwrap_or_default()
             })
             .collect();
-        let step_count = (delta_time * 600.0).floor().max(1.0).min(5.0);
+        let delta_time = time.delta_seconds();
+        let step_count = (delta_time * 300.0).floor().max(1.0).min(5.0);
         let step_time = delta_time / step_count;
         for _ in 0..step_count as i32 {
-            leader.snake.solve_followers(&mut positions,
-                |i| ((i + 1) as f32 * 0.1, (i + 1) as f32 * DISTANCE),
-                RADIUS,
-                RADIUS_HARD,
-                step_time * SPEED * 2.0
-            );
+            leader
+                .snake_head
+                .solve_body(&mut bodys, step_time, SPEED, RADIUS);
         }
-        for (i, e) in leader.followers.iter().enumerate() {
-            if let Ok(mut tm) = query_trans.get_mut(*e) {
-                tm.translation = positions[i].extend(0.0);
+        for (body, entity) in bodys.into_iter().zip(leader.followers.iter()) {
+            if let Ok((mut follower, mut tm)) = query_follower.get_mut(*entity) {
+                follower.snake_body = body.clone();
+                tm.translation = body.position.extend(0.0);
             }
         }
-    });
+    }
 }
 
-fn get_color(p: f32) -> Color {
-    let key = [
-        [1.0, 0.0, 0.0],
-        [1.0, 1.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 1.0, 1.0],
-        [0.0, 0.0, 1.0],
-        [1.0, 0.0, 1.0],
-        [1.0, 0.0, 0.0],
-    ];
-    let t = p * (key.len() - 1) as f32;
-    let index = t as usize;
-    let k = t - index as f32;
-    let mut rgb = [0.0; 3];
-    for i in 0..3 {
-        rgb[i] = key[index][i] * (1.0 - k) + key[index + 1][i] * k;
-    }
-    Color::rgb(rgb[0], rgb[1], rgb[2])
-}
-
-fn setup(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-    use bevy::render::mesh::{Indices, VertexAttributeValues};
-    let mut mesh = Mesh::new(bevy::render::pipeline::PrimitiveTopology::TriangleList);
-    let mut indices = Vec::new();
-    let mut vertices = vec![[0.0f32; 3]];
-    let normals = vec![[0.0, 0.0, 1.0]; 33];
-    let uvs = vec![[0.0; 2]; 33];
-    for i in 0..32 {
-        indices.push(0);
-        indices.push(i + 1);
-        indices.push(((i + 1) % 32) + 1);
-        let a = i as f32 * std::f32::consts::PI / 16.0;
-        vertices.push([a.sin() * RADIUS, a.cos() * RADIUS, 0.0]);
-    }
-    mesh.set_indices(Some(Indices::U32(indices)));
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        VertexAttributeValues::Float3(vertices),
-    );
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        VertexAttributeValues::Float3(normals),
-    );
-    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::Float2(uvs));
-    let circle = meshes.add(mesh);
-
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    let sprite_handle = assets.load("circle.png");
     let followers: Vec<_> = (1..10)
         .map(|i| {
             let pos = Vec2::new(i as f32 * -DISTANCE, 0.0);
             commands
                 .spawn_bundle(SpriteBundle {
-                    sprite: Sprite::new(Vec2::ONE),
-                    material: materials.add(get_color(i as f32 / 10.0).into()),
-                    mesh: circle.clone(),
+                    texture: sprite_handle.clone(),
                     transform: Transform::from_translation(pos.extend(0.0)),
+                    sprite: Sprite {
+                        color: Color::hsl(i as f32 * 36.0, 1.0, 0.5),
+                        ..Default::default()
+                    },
                     ..Default::default()
                 })
-                .insert(Follower::new())
+                .insert(Follower::new(i as f32 * 0.1, i as f32 * DISTANCE, pos))
                 .id()
         })
         .collect();
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands
         .spawn_bundle(SpriteBundle {
-            sprite: Sprite::new(Vec2::ONE),
-            material: materials.add(get_color(0.0).into()),
-            mesh: circle,
+            texture: sprite_handle.clone(),
+            sprite: Sprite {
+                color: Color::hsl(0.0, 1.0, 0.5),
+                ..Default::default()
+            },
             ..Default::default()
         })
         .insert(Leader::new(followers));
 }
 
 fn main() {
-    App::build()
-        .insert_resource(Msaa { samples: 4 })
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_system(update.system())
+        .add_startup_system(setup)
+        .add_system(leader_move)
+        .add_system(follower_move)
         .run();
 }
--- src/snake_move.rs
+++ src/snake_move.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::math::*;
 
 #[derive(Clone, Copy)]
 struct MoveRecord {
@@ -7,40 +7,31 @@ struct MoveRecord {
     position: Vec2,
 }
 
-pub struct SnakeMove {
+pub struct SnakeHead {
     records: Vec<MoveRecord>,
 }
 
-impl SnakeMove {
+impl SnakeHead {
     pub fn new() -> Self {
         Self {
-            records: Vec::new()
+            records: Vec::new(),
         }
     }
-    pub fn reset(&mut self, position: Vec2, dir: Vec2, time: f32) {
+    pub fn reset(&mut self, position: Vec2, time: f32) {
         self.records.clear();
-        self.records.push(MoveRecord {
-            time: -1e10,
-            distance: -10000.0,
-            position: position - dir.normalize_or_zero() * 10000.0,
-        });
-        self.records.push(MoveRecord {
-            time: -1e10,
-            distance: 0.0,
-            position,
-        });
         self.records.push(MoveRecord {
             time,
             distance: 0.0,
             position,
         });
+        self.records.push(self.records[0]);
     }
-    pub fn record(&mut self, position: Vec2, time: f32) {
-        if self.records.len() < 3 {
-            self.reset(position, Vec2::new(1.0, 0.0), time);
+    pub fn move_head(&mut self, position: Vec2, time: f32) {
+        if self.records.len() < 2 {
+            self.reset(position, time);
         }
-        let last0 = self.records[self.records.len()-1];
-        let last1 = self.records[self.records.len()-2];
+        let last0 = self.records[self.records.len() - 1];
+        let last1 = self.records[self.records.len() - 2];
         let dis1 = position.distance_squared(last1.position);
         if last0.distance - last1.distance < 1.0 && dis1 < 1.0 {
             self.records.pop();
@@ -56,158 +47,150 @@ impl SnakeMove {
                 position,
             });
         }
-        
+
         if self.records.len() > 2048 {
             self.records.drain(..128);
         }
     }
-    pub fn get_record(&self, delay: f32, distance: f32) -> Vec2 {
-        let time = self.records.last().unwrap().time - delay;
-        let find_time = match self
+    pub fn get_record(&self, delay: f32, distance: f32) -> (Vec2, f32) {
+        let last_rec = self.records.last().unwrap();
+        let time = last_rec.time - delay;
+
+        let time_index = match self
             .records
             .binary_search_by(|rec| rec.time.partial_cmp(&time).unwrap())
         {
             Ok(i) => i,
             Err(i) => i,
-        };
-        if find_time == 0 {
-            return self.records[0].position;
         }
-        let dis = if find_time < self.records.len() {
-            let record_prev = &self.records[find_time - 1];
-            let record_next = &self.records[find_time];
-            let k = (time - record_prev.time) / (record_next.time - record_prev.time);
-            k * (record_next.distance - record_prev.distance) + record_prev.distance - distance
-        } else {
-            self.records.last().unwrap().distance - distance
-        };
-        let find_dis = match self
+        .min(self.records.len() - 1);
+        if time_index == 0 {
+            return (self.records[0].position, distance);
+        }
+
+        let rec0 = &self.records[time_index - 1];
+        let rec1 = &self.records[time_index];
+        let k = (time - rec0.time) / (rec1.time - rec0.time);
+        let dis = k * (rec1.distance - rec0.distance) + rec0.distance - distance;
+        let dis_index = match self
             .records
             .binary_search_by(|rec| rec.distance.partial_cmp(&dis).unwrap())
         {
             Ok(i) => i,
             Err(i) => i,
-        };
-        if find_dis == 0 {
-            return self.records[0].position;
         }
-        if find_dis < self.records.len() {
-            let record_prev = &self.records[find_dis - 1];
-            let record_next = &self.records[find_dis];
-            let k = (dis - record_prev.distance) / (record_next.distance - record_prev.distance);
-            record_prev.position.lerp(record_next.position, k)
+        .min(self.records.len() - 1);
+        if dis_index > 0 {
+            let rec0 = &self.records[dis_index - 1];
+            let rec1 = &self.records[dis_index];
+            let k = (dis - rec0.distance) / (rec1.distance - rec0.distance);
+            (rec0.position.lerp(rec1.position, k), 0.0)
         } else {
-            self.records.last().unwrap().position
+            (self.records[0].position, self.records[0].distance - dis)
         }
     }
-    pub fn solve_followers<F>(
-        &self,
-        positions: &mut [Vec2],
-        delay_and_distance: F,
-        radius: f32,
-        hard_radius: f32,
-        max_move: f32)
-        where F: Fn(usize) -> (f32,f32) {
-        let leader_pos = self.records.last().unwrap().position;
-        let leader_dir = (leader_pos - self.get_record(0.0, 4.0)).normalize_or_zero();
-        
-        let mut target: Vec<_> = (0..positions.len())
-            .map(|i| {
-                let (delay, distance) = delay_and_distance(i);
-                self.get_record(delay, distance)
-            })
-            .collect();
-        let dir: Vec<_> = (0..positions.len())
-            .map(|i| {
-                let (delay, distance) = delay_and_distance(i);
-                (target[i] - self.get_record(delay, distance + 4.0)).normalize_or_zero()
-            })
-            .collect();
 
-        let mut hit = vec![false; target.len()];
+    pub fn solve_body(&self, body: &mut [SnakeBody], dt: f32, speed: f32, radius: f32) {
+        let max_move = dt * speed * 2.0;
+        let head_pos = self.records.last().unwrap().position;
 
-        for i in 0..target.len() {
-            let v = target[i] - leader_pos;
-            if v.length_squared() < radius * radius * 4.0 {
-                let dir0 = leader_dir;
-                let dir1 = dir[i];
-                if dir0.dot(v) > 0.0 && dir1.dot(v) < 0.0 {
-                    hit[i] = true;
-                    let len = v.length();
-                    let cross0 = dir0.x * v.y - dir0.y * v.x;
-                    let cross1 = dir1.x * v.y - dir1.y * v.x;
-                    let slide_len = (radius * radius * 4.0 - len * len).sqrt() + 0.1;
-                    let mut slide = Vec2::new(-v.y, v.x) * (slide_len / len);
-                    if cross0 > cross1 {
-                        slide *= -1.0;
+        let target: Vec<_> = body
+            .iter()
+            .map(|b| {
+                let (p, d) = self.get_record(b.delay, b.distance);
+                if d <= 0.0 {
+                    p
+                } else {
+                    let v = b.position - p;
+                    if v.length_squared() > d * d {
+                        p + v * (d / v.length())
+                    } else {
+                        b.position
                     }
-                    target[i] -= slide;
                 }
-            }
-        }
-        for i in 0..target.len() - 1 {
-            if hit[i] {
-                continue;
-            }
-            for j in i + 1..target.len() {
-                if hit[j] {
-                    continue;
+            })
+            .collect();
+        let mut positions_new: Vec<_> = body
+            .iter()
+            .zip(target.iter())
+            .map(|(b, t)| {
+                let mut pos = t.clone();
+                let v = pos - b.position;
+                if v.length_squared() > max_move * max_move {
+                    pos = b.position + v * (max_move / v.length());
                 }
-                let v = target[j] - target[i];
+                pos
+            })
+            .collect();
+
+        for _ in 0..4 {
+            for i in 0..body.len() {
+                let v = positions_new[i] - head_pos;
                 if v.length_squared() < radius * radius * 4.0 {
-                    let dir0 = dir[i];
-                    let dir1 = dir[j];
                     let len = v.length();
-                    if dir0.dot(v) > 0.0 && dir1.dot(v) < 0.0 {
-                        hit[i] = true;
-                        hit[j] = true;
-                        let cross0 = dir0.x * v.y - dir0.y * v.x;
-                        let cross1 = dir1.x * v.y - dir1.y * v.x;
-                        let slide_len = (radius * radius * 4.0 - len * len).sqrt() * 0.5 + 0.1;
-                        let mut slide = Vec2::new(-v.y, v.x) * (slide_len / len);
-                        if cross0 > cross1 {
-                            slide *= -1.0;
+                    let pop_dis = (radius * 2.0 - len).min(max_move);
+                    if v.dot(target[i] - head_pos) < -0.001 {
+                        let mut angle = pop_dis * 0.5 / radius;
+                        let dir = target[i] - positions_new[i];
+                        let cross = dir.x * v.y - dir.y * v.x;
+                        if 0.0 < cross {
+                            angle = -angle;
                         }
-                        target[i] += slide;
-                        target[j] -= slide;
-                        break;
+                        let offset = Mat2::from_angle(angle).mul_vec2(v) * (1.0 + pop_dis / len);
+                        positions_new[i] = head_pos + offset;
+                    } else {
+                        positions_new[i] += v * (pop_dis / len);
+                    }
+                }
+            }
+            for i in 0..body.len() - 1 {
+                for j in i + 1..body.len() {
+                    let v = positions_new[j] - positions_new[i];
+                    if v.length_squared() < radius * radius * 4.0 {
+                        let len = v.length();
+                        let pop_dis = (radius - len * 0.5).min(max_move);
+                        if v.dot(target[j] - target[i]) < -0.001 {
+                            let mut angle = pop_dis / radius;
+                            let dir0 = target[i] - positions_new[i];
+                            let dir1 = target[j] - positions_new[j];
+                            let cross0 = dir0.x * v.y - dir0.y * v.x;
+                            let cross1 = dir1.x * v.y - dir1.y * v.x;
+                            if cross0 < cross1 {
+                                angle = -angle;
+                            }
+                            let offset =
+                                Mat2::from_angle(angle).mul_vec2(v) * (0.5 + pop_dis / len);
+                            let center = (positions_new[i] + positions_new[j]) * 0.5;
+                            positions_new[i] = center - offset;
+                            positions_new[j] = center + offset;
+                        } else {
+                            let pop = v * (pop_dis / len);
+                            positions_new[i] -= pop;
+                            positions_new[j] += pop;
+                        }
                     }
                 }
             }
         }
-        
-        for i in 0..positions.len() {
-            if target[i].distance_squared(leader_pos) < hard_radius * hard_radius * 0.25 {
-                continue;
-            }
-            let delta = target[i] - positions[i];
-            let delta_len_sq = delta.length_squared();
-            if delta_len_sq <= max_move * max_move {
-                positions[i] = target[i];
-            } else {
-                positions[i] += delta * (max_move / delta_len_sq.sqrt());
-            }
-
-            let to_leader = positions[i] - leader_pos;
-            let len_sq = to_leader.length_squared();
-            if len_sq < hard_radius * hard_radius * 4.0 {
-                let len = len_sq.sqrt();
-                let d = (hard_radius - len * 0.5).min(max_move);
-                positions[i] += to_leader * (d / len);
-            }
+        for i in 0..body.len() {
+            body[i].position = positions_new[i];
         }
-        for i in 0..positions.len() - 1 {
-            for j in i + 1..positions.len() {
-                let v = positions[j] - positions[i];
-                let len_sq = v.length_squared();
-                if len_sq < hard_radius * hard_radius * 4.0 && len_sq > 0.001{
-                    let len = len_sq.sqrt();
-                    let d = (hard_radius - len * 0.5).min(max_move);
-                    let pop = v * (d / len);
-                    positions[j] += pop;
-                    positions[i] -= pop;
-                }
-            }
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct SnakeBody {
+    delay: f32,
+    distance: f32,
+    pub position: Vec2,
+}
+
+impl SnakeBody {
+    pub fn new(delay: f32, distance: f32, position: Vec2) -> Self {
+        Self {
+            delay,
+            distance,
+            position,
         }
     }
 }
